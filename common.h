bac901023061ecfdca88fff3ccbb7c02553b9417
Roberto De Ioris <roberto@unbit.it>
2014-05-17 05:44:49 +0200

    added QThreads support

--- common.h
+++ common.h
@@ -2,15 +2,33 @@
 #include <QtCore/QCoreApplication>
 #include <QSocketNotifier>
 #include <QThread>
+#include <QMutex>
+#include <QWaitCondition>
 
-class RequestHandler : public QObject {
+class uWSGICoreHandler : public QObject {
         Q_OBJECT
         public:
-                RequestHandler(struct wsgi_request *);
-        private slots:
+                uWSGICoreHandler(struct wsgi_request *, int);
+        public slots:
                 void handle_request(int);
                 void handle_signal(int);
         public:
                 struct wsgi_request *wsgi_req;
+		int core_id;
 };
 
+class uWSGIThreadsDispatcher : public QObject {
+        Q_OBJECT
+        private slots:
+                void request_dispatch(int);
+                void signal_dispatch(int);
+};
+
+
+class uWSGIThread : public QThread {
+	Q_OBJECT
+	public:
+		void run();
+        public:
+                uWSGICoreHandler *uch;
+};
--- qtloop.cc
+++ qtloop.cc
@@ -2,17 +2,99 @@
 
 extern struct uwsgi_server uwsgi;
 
-RequestHandler::RequestHandler(struct wsgi_request *wr) {
+#define UWSGI_QT_EVENT_NULL	0
+#define UWSGI_QT_EVENT_REQUEST	1
+#define UWSGI_QT_EVENT_SIGNAL	2
+
+QWaitCondition QueueReady;
+QWaitCondition QueueFree;
+QMutex mutex;
+
+struct qt_uwsgi_status {
+	int event;
+	int fd;
+} qt_uwsgi_status;
+
+void uWSGIThread::run() {
+
+	uwsgi_setup_thread_req(uch->core_id, uch->wsgi_req);
+
+	while (uwsgi.workers[uwsgi.mywid].manage_next_request) {
+		mutex.lock();
+		if (qt_uwsgi_status.event == UWSGI_QT_EVENT_NULL) {
+			QueueReady.wait(&mutex);
+		}
+
+		// this is required when using wakeAll()
+		if (qt_uwsgi_status.event == UWSGI_QT_EVENT_NULL) {
+			mutex.unlock();
+			continue;
+		}
+
+		struct qt_uwsgi_status qitem;
+		qitem.event = qt_uwsgi_status.event;
+		qitem.fd = qt_uwsgi_status.fd;
+
+		qt_uwsgi_status.event = UWSGI_QT_EVENT_NULL;
+
+		QueueFree.wakeOne();
+	
+		mutex.unlock();
+
+		// run the handler
+
+		if (qitem.event == UWSGI_QT_EVENT_REQUEST) {
+			uch->handle_request(qitem.fd);
+		}
+		else if (qitem.event == UWSGI_QT_EVENT_SIGNAL) {
+			uch->handle_signal(qitem.fd);
+		}
+		
+	}
+}
+
+void uWSGIThreadsDispatcher::request_dispatch(int fd) {
+	mutex.lock();
+	if (qt_uwsgi_status.event > UWSGI_QT_EVENT_NULL) {
+		QueueFree.wait(&mutex);	
+	}
+	qt_uwsgi_status.event = UWSGI_QT_EVENT_REQUEST;
+	qt_uwsgi_status.fd = fd;
+
+	QueueReady.wakeOne();
+
+	mutex.unlock();
+	
+}
+
+void uWSGIThreadsDispatcher::signal_dispatch(int fd) {
+        mutex.lock();
+        if (qt_uwsgi_status.event > UWSGI_QT_EVENT_NULL) {
+                QueueFree.wait(&mutex);
+        }
+
+        qt_uwsgi_status.event = UWSGI_QT_EVENT_SIGNAL;
+        qt_uwsgi_status.fd = fd;
+
+        QueueReady.wakeOne();
+
+        mutex.unlock();
+
+}
+
+
+uWSGICoreHandler::uWSGICoreHandler(struct wsgi_request *wr, int core) {
 	wsgi_req = wr;
+	core_id = core;
 }
 
 // manage signal handlers
-void RequestHandler::handle_signal(int fd) {
+void uWSGICoreHandler::handle_signal(int fd) {
 	uwsgi_receive_signal(fd, (char *) "worker", uwsgi.mywid);
 }
 
 // manage requests
-void RequestHandler::handle_request(int fd) {
+void uWSGICoreHandler::handle_request(int fd) {
 	struct uwsgi_socket *uwsgi_sock = uwsgi.sockets;
         while(uwsgi_sock) {
                 if (uwsgi_sock->fd == fd) break;
@@ -75,27 +157,60 @@ end:
 extern "C" void qtloop_loop() {
 	// ensure SIGPIPE is ignored
         signal(SIGPIPE, SIG_IGN);
+
+	// initialize Qt app
 	QCoreApplication app(uwsgi.argc, uwsgi.argv);
 
-        // create a QObject (you need one for each virtual core)
-        RequestHandler *rh = new RequestHandler(&uwsgi.workers[uwsgi.mywid].cores[0].req);
+        // create a QObject (you need one for each thread, async mode is not supported)
+	if (uwsgi.threads > 1) {
+		int i;
+		// the first thread is used for the event loop
+		for(i=1;i<uwsgi.threads;i++) {
+			uWSGIThread *t = new uWSGIThread();
+			uWSGICoreHandler *uch = new uWSGICoreHandler(&uwsgi.workers[uwsgi.mywid].cores[i].req, i);
+			t->uch = uch;
+			t->start();
+		}
 
-        // monitor signals
-	if (uwsgi.signal_socket > -1) {
-		QSocketNotifier *signal_qsn = new QSocketNotifier(uwsgi.signal_socket, QSocketNotifier::Read, &app);
-		QObject::connect(signal_qsn, SIGNAL(activated(int)), rh, SLOT(handle_signal(int)));
+		uWSGIThreadsDispatcher *uts = new uWSGIThreadsDispatcher();
 
-		QSocketNotifier *my_signal_qsn = new QSocketNotifier(uwsgi.my_signal_socket, QSocketNotifier::Read, &app);
-		QObject::connect(my_signal_qsn, SIGNAL(activated(int)), rh, SLOT(handle_signal(int)));
+		// monitor signals
+                if (uwsgi.signal_socket > -1) {
+                        QSocketNotifier *signal_qsn = new QSocketNotifier(uwsgi.signal_socket, QSocketNotifier::Read, &app);
+                        QObject::connect(signal_qsn, SIGNAL(activated(int)), uts, SLOT(signal_dispatch(int)));
+
+                        QSocketNotifier *my_signal_qsn = new QSocketNotifier(uwsgi.my_signal_socket, QSocketNotifier::Read, &app);
+                        QObject::connect(my_signal_qsn, SIGNAL(activated(int)), uts, SLOT(signal_dispatch(int)));
+                }
+
+                // monitor sockets
+                struct uwsgi_socket *uwsgi_sock = uwsgi.sockets;
+                while(uwsgi_sock) {
+                        QSocketNotifier *qsn = new QSocketNotifier(uwsgi_sock->fd, QSocketNotifier::Read, &app);
+                        QObject::connect(qsn, SIGNAL(activated(int)), uts, SLOT(request_dispatch(int)));
+                        uwsgi_sock = uwsgi_sock->next;
+                }
 	}
+	else {
+        	uWSGICoreHandler *uch = new uWSGICoreHandler(&uwsgi.workers[uwsgi.mywid].cores[0].req, 0);
 
-        // monitor sockets
-        struct uwsgi_socket *uwsgi_sock = uwsgi.sockets;
-        while(uwsgi_sock) {
-                QSocketNotifier *qsn = new QSocketNotifier(uwsgi_sock->fd, QSocketNotifier::Read, &app);
-                QObject::connect(qsn, SIGNAL(activated(int)), rh, SLOT(handle_request(int)));
-                uwsgi_sock = uwsgi_sock->next;
-        }
+        	// monitor signals
+		if (uwsgi.signal_socket > -1) {
+			QSocketNotifier *signal_qsn = new QSocketNotifier(uwsgi.signal_socket, QSocketNotifier::Read, &app);
+			QObject::connect(signal_qsn, SIGNAL(activated(int)), uch, SLOT(handle_signal(int)));
+
+			QSocketNotifier *my_signal_qsn = new QSocketNotifier(uwsgi.my_signal_socket, QSocketNotifier::Read, &app);
+			QObject::connect(my_signal_qsn, SIGNAL(activated(int)), uch, SLOT(handle_signal(int)));
+		}
+
+        	// monitor sockets
+        	struct uwsgi_socket *uwsgi_sock = uwsgi.sockets;
+        	while(uwsgi_sock) {
+                	QSocketNotifier *qsn = new QSocketNotifier(uwsgi_sock->fd, QSocketNotifier::Read, &app);
+                	QObject::connect(qsn, SIGNAL(activated(int)), uch, SLOT(handle_request(int)));
+                	uwsgi_sock = uwsgi_sock->next;
+        	}
+	}
 
         // start the qt event loop
         app.exec();
